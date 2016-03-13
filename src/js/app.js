b9ade8efb35ef480f0adcc1dcd2d038dffd338e0
kellim <kelli.blalock2@gmail.com>
2016-03-12 20:15:25 -0800

    feat: Populate results list after search
    Significant refactoring was also done to put the functions
    that load API data into AppViewModel to get it to work with
    the asynchronous AJAX code.

--- src/js/app.js
+++ src/js/app.js
@@ -1,47 +1,80 @@
-
+var Market = function(data) {
+  this.marketName = ko.observable(data.marketName);
+  this.address = ko.observable(data.address);
+  this.schedule = ko.observable(data.schedule);
+  this.products = ko.observable(data.products);
+  this.latitude = ko.observable(data.latitude);
+  this.longitude = ko.observable(data.longitude);
+}
 
 var AppViewModel = function() {
   var self = this;
   self.zip = ko.observable('');
+  self.marketList = ko.observableArray();
+  self.loadMarketsError = ko.observable('');
+  self.loadMarketDetailsError = ko.observable('');
 
-  this.getMarketData = function() {
-  console.log(this.zip());
-  var marketDataArr = [];
-  var marketsURL = "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + this.zip();
-    $.ajax({
-        type: "GET",
-        contentType: "application/json; charset=utf-8",
-        url: marketsURL,
-        dataType: 'jsonp',
-        success: function(data) {
-          var id, name, address, schedule, googleLink, products;
-          $.each(data.results, function(index, marketData) {
-            name = marketData.marketname;
-            console.log(marketData.marketname);
-            getMarketDetails(marketData, name);
-          });
-        }
+  // Called when AJAX function in loadMarketDetails() is successful
+  // to add a marketItem with data from the API to marketList()
+  self.createMarketItem = function(marketItem) {
+    self.marketList.push(new Market(marketItem));
+    console.log(self.marketList());
+  }
+
+  // Gets market name and ID data from Farmer's Market API
+  // and calls loadMarketDetails() to get each market's full details.
+  this.loadMarkets = function() {
+    self.loadMarketsError = "";
+    var loadData = $.ajax({
+      type: "GET",
+      contentType: "application/json; charset=utf-8",
+      url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + this.zip(),
+      dataType: 'jsonp'
     });
-
-    getMarketDetails = function(marketData, name) {
-      id = marketData.id;
-      $.ajax({
-        type: "GET",
-        contentType: "application/json; charset=utf-8",
-        url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id,
-        dataType: 'jsonp',
-        success: function(marketDetails) {
-          googleLink = marketDetails.marketdetails.GoogleLink;
-          address = marketDetails.marketdetails.Address;
-          schedule = marketDetails.marketdetails.Schedule;
-          products = marketDetails.marketdetails.Products;
-          marketDataArr.push({'name' : name, 'googleLink' : googleLink, 'address' : address, 'schedule' : schedule,
-                            'products' : products});
-          console.log(marketDataArr);
-        }
+    loadData.done(function(data) {
+      var id, marketName;
+      $.each(data.results, function(i, marketData) {
+        marketName = marketData.marketname;
+        console.log(marketData.marketname);
+        marketId = marketData.id;
+        // Remove number (distance) preceding name in data returned by API
+        marketName = marketName.slice(marketName.indexOf(' ') + 1);
+        self.loadMarketDetails(marketId, marketName);
       });
-    }
+    });
+    loadData.fail(function() {
+      self.loadMarketsError = "Error Loading Market Data."
+    });
+  };
+
+  // Called by loadMarkets() to get each market's full details since the initial
+  // AJAX call only provides a market's name and ID. This function gets the
+  // address, schedule, products, latitude and longitude and updates
+  // self.marketDetails[].
+  this.loadMarketDetails = function(marketId, marketName) {
+    var loadDetails = $.ajax({
+      type: "GET",
+      contentType: "application/json; charset=utf-8",
+      url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id="
+      + marketId,
+      dataType: 'jsonp'
+    });
+    loadDetails.done(function(details) {
+      var address, schedule, googleLink, products, latitude, longitude;
+      googleLink = decodeURIComponent(details.marketdetails.GoogleLink);
+      address = details.marketdetails.Address;
+      schedule = details.marketdetails.Schedule;
+      products = details.marketdetails.Products;
+      // Extract latitude and longitude from Google link returned by API.
+      latitude = googleLink.slice(googleLink.indexOf('=') + 1, googleLink.indexOf(','));
+      longitude = googleLink.slice(googleLink.indexOf(',') + 2, googleLink.indexOf('(') -1);
+      var marketItem = {'marketId' : marketId, 'marketName' : marketName, 'latitude' : latitude, 'longitude' : longitude,
+                        'address' : address, 'schedule' : schedule, 'products' : products};
+    self.createMarketItem(marketItem);
+    });
+    loadDetails.fail(function() {
+      self.loadMarketDetailsError = "Error Loading Market Details."
+    });
   }
 }
-
 ko.applyBindings(new AppViewModel());
